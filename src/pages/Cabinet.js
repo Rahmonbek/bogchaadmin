d4eb3efe4f91b67f8f36a9e30d8c3fdaff7c4877
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-10-18 14:36:30 +0500

    svmd

--- src/pages/Cabinet.js
+++ src/pages/Cabinet.js
@@ -582,12 +582,14 @@ export default class Cabinet extends Component {
               position: "fixed",
               right: "0",
               bottom: "50px",
+              backgroundColor:'darkblue',
+              boxShadow: "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
               paddingLeft: "30px",
               paddingRight: "30px",
             }}
             onClick={this.editKinderGarden}
           >
-            Saqlash
+            Ma'lumotlarni saqlash
           </Button>
         </Container>
       </div>
